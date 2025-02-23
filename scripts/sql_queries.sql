8b6c601f0babb7b634a0dad592d77bab92ee08c0
MSalopek <matija.salopek994@gmail.com>
2025-02-23 18:56:29 +0100

    add sql query for daily revenue

--- scripts/sql_queries.sql
+++ scripts/sql_queries.sql
@@ -12,3 +12,5 @@ select count(*), filler, source_domain from tx_data where ingestion_timestamp > 
 select count(*), filler, source_domain from tx_data where ingestion_timestamp > DATETIME('now', '-1 day') and filler = 'osmo1xjuvq8mlmhc24l2ewya2uyyj9t6r0dcfdhza6h' group by source_domain;
 select count(*), filler, source_domain from tx_data where height >= 30097705 and filler = 'osmo1xjuvq8mlmhc24l2ewya2uyyj9t6r0dcfdhza6h' group by source_domain;
 select count(*), filler, source_domain from tx_data where height >= 30097705 and filler = 'osmo153ly6vgjgk3fvh624a3d0waa53wycyayxl0k4w' group by source_domain;
+
+SELECT DATE(b.datetime) as day, SUM(t.solver_revenue)/1000000 as total_revenue FROM tx_data t JOIN osmo_block_times b ON t.height = b.height WHERE t.filler = 'osmo1xjuvq8mlmhc24l2ewya2uyyj9t6r0dcfdhza6h' GROUP BY DATE(b.datetime) HAVING total_revenue > 0 ORDER BY total_revenue DESC;
